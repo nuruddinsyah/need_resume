10834b2d86cace57df5dc61d9686913f2016dd2c
Muhammad Faruq Nuruddinsyah <faruq.nuruddinsyah@gmail.com>
2020-11-05 22:51:19 +0700

    Update to version 1.0.5+1

--- example/another_example.dart
+++ example/another_example.dart
@@ -4,70 +4,68 @@ import 'package:need_resume/need_resume.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'My Application',
-            home: HomeScreen()
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'My Application', home: HomeScreen());
+  }
 }
 
 class HomeScreen extends StatefulWidget {
-    @override
-    HomeScreenState createState() => HomeScreenState();
+  @override
+  HomeScreenState createState() => HomeScreenState();
 }
 
 class HomeScreenState extends ResumableState<HomeScreen> {
-    @override
-    void onResume() {
-        switch (resume.source) {
-            case 'another_screen':
-                print('Data from AnotherScreen: ${resume.data}');
-                break;
-        }
+  @override
+  void onResume() {
+    switch (resume.source) {
+      case 'another_screen':
+        print('Data from AnotherScreen: ${resume.data}');
+        break;
     }
+  }
 
-    void goAnotherScreen() {
-        push(context, MaterialPageRoute(builder: (context) => AnotherScreen()), 'another_screen');
-    }
+  void goAnotherScreen() {
+    push(context, MaterialPageRoute(builder: (context) => AnotherScreen()),
+        'another_screen');
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: Center(
-                child: RaisedButton(
-                    child: Text('Go to Another Screen'),
-                    onPressed: () {
-                        goAnotherScreen();
-                    },
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to Another Screen'),
+          onPressed: () {
+            goAnotherScreen();
+          },
+        ),
+      ),
+    );
+  }
 }
 
 class AnotherScreen extends StatefulWidget {
-    @override
-    AnotherScreenState createState() => AnotherScreenState();
+  @override
+  AnotherScreenState createState() => AnotherScreenState();
 }
 
 class AnotherScreenState extends ResumableState<AnotherScreen> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: Center(
-                child: RaisedButton(
-                    child: Text('Go Back'),
-                    onPressed: () {
-                        goBack();
-                    },
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go Back'),
+          onPressed: () {
+            goBack();
+          },
+        ),
+      ),
+    );
+  }
 
-    void goBack() {
-        Navigator.pop(context, 'Hello!');
-    }
+  void goBack() {
+    Navigator.pop(context, 'Hello!');
+  }
 }
--- example/example.dart
+++ example/example.dart
@@ -4,105 +4,102 @@ import 'package:need_resume/need_resume.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'My Application',
-            home: HomeScreen()
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'My Application', home: HomeScreen());
+  }
 }
 
 class HomeScreen extends StatefulWidget {
-    @override
-    HomeScreenState createState() => HomeScreenState();
+  @override
+  HomeScreenState createState() => HomeScreenState();
 }
 
 class HomeScreenState extends ResumableState<HomeScreen> {
-    @override
-    void onReady() {
-        // Implement your code inside here
+  @override
+  void onReady() {
+    // Implement your code inside here
 
-        print('HomeScreen is ready!');
-    }
+    print('HomeScreen is ready!');
+  }
 
-    @override
-    void onResume() {
-        // Implement your code inside here
+  @override
+  void onResume() {
+    // Implement your code inside here
 
-        print('HomeScreen is resumed!');
-    }
+    print('HomeScreen is resumed!');
+  }
 
-    @override
-    void onPause() {
-        // Implement your code inside here
+  @override
+  void onPause() {
+    // Implement your code inside here
 
-        print('HomeScreen is paused!');
-    }
+    print('HomeScreen is paused!');
+  }
 
-    void goAnotherScreen() {
-        // Replace Navigator.push() or Navigator.pushNamed() with push() or pushNamed()
+  void goAnotherScreen() {
+    // Replace Navigator.push() or Navigator.pushNamed() with push() or pushNamed()
 
-        push(context, MaterialPageRoute(builder: (context) => AnotherScreen()));
-    }
+    push(context, MaterialPageRoute(builder: (context) => AnotherScreen()));
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: Center(
-                child: RaisedButton(
-                    child: Text('Go to Another Screen'),
-                    onPressed: () {
-                        goAnotherScreen();
-                    },
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to Another Screen'),
+          onPressed: () {
+            goAnotherScreen();
+          },
+        ),
+      ),
+    );
+  }
 }
 
 class AnotherScreen extends StatefulWidget {
-    @override
-    AnotherScreenState createState() => AnotherScreenState();
+  @override
+  AnotherScreenState createState() => AnotherScreenState();
 }
 
 class AnotherScreenState extends ResumableState<AnotherScreen> {
-    @override
-    void onReady() {
-        // Implement your code inside here
+  @override
+  void onReady() {
+    // Implement your code inside here
 
-        print('AnotherScreen is ready!');
-    }
+    print('AnotherScreen is ready!');
+  }
 
-    @override
-    void onResume() {
-        // Implement your code inside here
+  @override
+  void onResume() {
+    // Implement your code inside here
 
-        print('AnotherScreen is resumed!');
-    }
+    print('AnotherScreen is resumed!');
+  }
 
-    @override
-    void onPause() {
-        // Implement your code inside here
+  @override
+  void onPause() {
+    // Implement your code inside here
 
-        print('AnotherScreen is paused!');
-    }
+    print('AnotherScreen is paused!');
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: Center(
-                child: RaisedButton(
-                    child: Text('Go Back'),
-                    onPressed: () {
-                        goBack();
-                    },
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go Back'),
+          onPressed: () {
+            goBack();
+          },
+        ),
+      ),
+    );
+  }
 
-    void goBack() {
-        Navigator.pop(context);
-    }
+  void goBack() {
+    Navigator.pop(context);
+  }
 }
